312c670146d6929b8b80928e1ea9941378c8321d
austine94 <63097423+austine94@users.noreply.github.com>
2023-05-31 18:22:49 +0100

    Add files via upload

--- Review_Sims/main.R
+++ Review_Sims/main.R
@@ -5,6 +5,5 @@
 #Run this to generate all the tables from the FAST Paper. These will be stored in the results folder.
 
 source("fast_script.R")
-source("./False Alarms/Prop1_Threshold.R")
-source("./Power & Delay/All_Scenarios_Power_Delay.R")
-#source("./ROC Curves/GP_ROC.R")
+source("./Power & Delay/All_Scenarios_Power_Delay_NEW.R")
+source("./Varying_m/Varying_m.R")
