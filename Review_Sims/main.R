c40abf81258428b87e59cc22f07ccd4a97ab8e21
austine94 <63097423+austine94@users.noreply.github.com>
2023-06-07 17:44:46 +0100

    Add files via upload

---
+++ Review_Sims/main.R
@@ -0,0 +1,14 @@
+######
+#Main Function
+######
+
+#Run this to generate all the tables from the FAST Paper. These will be stored in the results folder.
+
+source("fast_script.R")
+source("comparative_methods.R")
+source("./Power & Delay/All_Scenarios_Partial_Anomaly_Period.R")
+source("./Power & Delay/All_Scenarios_Full_Anomaly_Period.R")
+source("./Power & Delay/Anomaly_At_End.R")
+#source("./Power & Delay/Varying_Anomaly_Length.R")
+source("./Choice of ODE Order/Varying_m.R")
+source("./Choice of ODE Order/BIC_Consistency.R")
